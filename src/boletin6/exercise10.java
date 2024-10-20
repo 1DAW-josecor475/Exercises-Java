fccff1f51416a72361a711798c8d89449e2f9e97
1DAW-josecor475 <jcorfer910@g.educaand.es>
2024-10-20 13:07:50 +0200

    exercise10 done

--- src/boletin6/exercise10.java
+++ src/boletin6/exercise10.java
@@ -2,10 +2,19 @@
 * Introducir un número de cualquier cifra y mostrar la suma de sus cifras.
 */
 package boletin6;
+import java.util.Scanner;
 
-// TODO
 public class exercise10 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		System.out.println("Introduzca un número: ");
+		long userNumber = sc.nextLong();
+		int allDigitsSum = 0;
 
+		while (userNumber > 0) {
+			allDigitsSum += userNumber % 10;    // Sum last digit of userNumber
+			userNumber /= 10;    // Remove last digit of userNumber
+		}
+		System.out.println("La suma de los dígitos es: " + allDigitsSum);
     }
 }
